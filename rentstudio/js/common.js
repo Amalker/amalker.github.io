6cdfe1b11fcfbfb66813720e380611fc59149172
amalker <amalker@bigmir.net>
2018-07-20 21:46:57 +0300

    yty

--- rentstudio/js/common.js
+++ rentstudio/js/common.js
@@ -72,7 +72,49 @@ $('#inline-popups').magnificPopup({
   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
 	
-	
+$('.partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,	
+		autoplay: true,
+			autoplaySpeed: 1500,
+			autoplayTimeout: 3000,
+			autoplayHoverPause: true,			
+			dotsSpeed: 4000,
+			dragEndSpeed: 2500,	
+		responsive: {
+			0: {
+				items: 2
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});	
+
+$('.image-popup').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 800 // don't foget to change the duration also in CSS
+		}
+	});
+
 
 $("select").selectize();
 
